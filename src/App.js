b1580683ad5a9b5227b818d12fbc8d7940731de1
oscarvedo <balnicor@gmail.com>
2022-06-07 21:24:49 -0500

    Class Component: componentDidMount implementation for fetching monsters and searching/filtering functions

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import { Component } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  //Initialice the component and local state
   constructor() {
     super();
 
     this.state = {
-      name: 'Yihua'
-    }
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  //Right after the first render - API Requests here
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return {monsters: users}
+      }));
+  }
+
+  onSearchChange = (e) => { //Performance Optimization. Declared once.
+    const searchString = e.target.value.toLowerCase();
+    this.setState(
+      () => {
+        return { searchField: searchString };
+      }
+    );
   }
 
   render() {
+    let { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button 
-            onClick={() => {
-              this.setState({ name: 'Oscar' });
-            }}>
-              Change name
-            </button>
-        </header>
+      <input 
+        className='search-box' 
+        type='search' 
+        placeholder='Search monsters'
+        onChange={onSearchChange}
+      />
+      {
+        filteredMonsters.map(monster => (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        ))
+      }
       </div>
     );
   }
